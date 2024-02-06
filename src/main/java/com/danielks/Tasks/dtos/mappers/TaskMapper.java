677b7bbc3d81c1fe8d7275dd6a848ad06f90d37b
dschifel <danielschifelbain@gmail.com>
2024-02-06 14:55:47 -0300

    Fixed: add component model + created conversion to entity

--- src/main/java/com/danielks/Tasks/dtos/mappers/TaskMapper.java
+++ src/main/java/com/danielks/Tasks/dtos/mappers/TaskMapper.java
@@ -6,17 +6,15 @@ import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
-@Mapper
+@Mapper(componentModel = "spring")
 public interface TaskMapper {
     TaskMapper INSTANCE = Mappers.getMapper(TaskMapper.class);
 
-    @Mapping(target = "id", source = "id")
     @Mapping(target = "header", source = "header")
     @Mapping(target = "body", source = "body")
     @Mapping(target = "ended", source = "ended")
     TaskDTO taskToTaskDTO(Task task);
 
-    @Mapping(target = "id", source = "id")
     @Mapping(target = "header", source = "header")
     @Mapping(target = "body", source = "body")
     @Mapping(target = "ended", source = "ended")
