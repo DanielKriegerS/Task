13683dadfc41159728e4ed63c63c7c50428d9979
dschifel <danielschifelbain@gmail.com>
2024-02-06 09:58:56 -0300

    Fixed: implemented new convertion method on mapper

--- src/main/java/com/danielks/Tasks/dtos/mappers/TaskMapper.java
+++ src/main/java/com/danielks/Tasks/dtos/mappers/TaskMapper.java
@@ -15,4 +15,10 @@ public interface TaskMapper {
     @Mapping(target = "body", source = "body")
     @Mapping(target = "ended", source = "ended")
     TaskDTO taskToTaskDTO(Task task);
+
+    @Mapping(target = "id", source = "id")
+    @Mapping(target = "header", source = "header")
+    @Mapping(target = "body", source = "body")
+    @Mapping(target = "ended", source = "ended")
+    Task taskDTOToTask(TaskDTO taskDTO);
 }
