0a77c9dc068329db133fd234541f2628cfe3f412
dschifel <danielschifelbain@gmail.com>
2024-02-06 09:50:18 -0300

    New: created Task Mapper interface

---
+++ src/main/java/com/danielks/Tasks/dtos/mappers/TaskMapper.java
@@ -0,0 +1,18 @@
+package com.danielks.Tasks.dtos.mappers;
+
+import com.danielks.Tasks.dtos.TaskDTO;
+import com.danielks.Tasks.entities.Task;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface TaskMapper {
+    TaskMapper INSTANCE = Mappers.getMapper(TaskMapper.class);
+
+    @Mapping(target = "id", source = "id")
+    @Mapping(target = "header", source = "header")
+    @Mapping(target = "body", source = "body")
+    @Mapping(target = "ended", source = "ended")
+    TaskDTO taskToTaskDTO(Task task);
+}
